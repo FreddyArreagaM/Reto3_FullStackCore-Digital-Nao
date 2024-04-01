89a657cc1c49cafb91a1cf6a280133b3ce7d29ca
Freddy Arreaga Morán <127709400+FreddyArreagaM@users.noreply.github.com>
2024-03-31 21:13:22 -0500

    commit v6 implements CI

--- karma.conf.ts
+++ karma.conf.ts
@@ -34,11 +34,11 @@ module.exports = function (config) {
       colors: true,
       logLevel: config.LOG_INFO,
       autoWatch: true,
-      browsers: ['Chrome'],
+      browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
       customLaunchers: {
         ChromeHeadlessCI: {
-          base: 'Chrome',
-          flags: ['--no-sandbox']
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox', '--disable-gpu']
         }
       },
       singleRun: false,
