4729a8c9a9531471c33e846b961826e526083996
Freddy Arreaga Morán <127709400+FreddyArreagaM@users.noreply.github.com>
2024-04-01 08:53:07 -0500

    Commit v3 Implementation CI Angular Test

--- src/app/mathFunction.spec.ts
+++ src/app/mathFunction.spec.ts
@@ -1,11 +1,12 @@
 import { dividir } from "./mathFunction";
 import { multiplicar } from "./mathFunction";
 
-//Prueba de división de números
+//Prueba de división de dos números
 it('should return 2 with 6 / 3', () =>{
     expect(dividir(6,3)).toEqual(2);
 })
 
+//Prueba de multiplicación de dos números
 it('should return 10 with 5 * 2', ()=>{
     expect(multiplicar(5,2)).toEqual(10);
 })
