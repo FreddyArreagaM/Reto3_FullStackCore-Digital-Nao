10a7b8cc3beeb9a2180fcd79183f141b343c3df1
Freddy Arreaga Morán <127709400+FreddyArreagaM@users.noreply.github.com>
2024-03-31 21:04:59 -0500

    commit v5 integration ci

--- karma.conf.ts
+++ karma.conf.ts
@@ -34,10 +34,10 @@ module.exports = function (config) {
       colors: true,
       logLevel: config.LOG_INFO,
       autoWatch: true,
-      browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+      browsers: ['Chrome'],
       customLaunchers: {
         ChromeHeadlessCI: {
-          base: 'ChromeHeadless',
+          base: 'Chrome',
           flags: ['--no-sandbox']
         }
       },
